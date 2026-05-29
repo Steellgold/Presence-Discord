ebaf64c8ed17e6c3e18dd5e52367045fdfa8904f
Gaëtan H <me@gaetanhus.fr>
2026-05-29 22:47:11 +0200

    Remove Nginx configuration and Railway deployment settings

--- web/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-# Substitute ONLY $PORT — leaving nginx variables ($uri, $host, etc.) intact
-envsubst '$PORT' < /tmp/nginx.conf.template > /etc/nginx/conf.d/default.conf
-exec nginx -g 'daemon off;'
