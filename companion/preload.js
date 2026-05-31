5870ce6ae0731a1d1e2f9473afc532dc0211ff36
Gaëtan H <me@gaetanhus.fr>
2026-05-31 02:03:40 +0200

    Remove all support for old app

--- companion/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  onLog:          cb => ipcRenderer.on("log",        (_, v) => cb(v)),
-  onLogs:         cb => ipcRenderer.on("logs-batch", (_, v) => cb(v)),
-  onState:        cb => ipcRenderer.on("state",      (_, v) => cb(v)),
-  closeWindow:    ()  => ipcRenderer.send("close-window"),
-  minimizeWindow: ()  => ipcRenderer.send("minimize-window"),
-});
